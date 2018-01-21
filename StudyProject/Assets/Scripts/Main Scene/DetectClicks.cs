0abe9e4b4a67513c8f32fabdd4d6705cbf8bb00c
Yachnik Alexander <sansanich230899@gmail.com>
2018-01-21 16:14:49 +0300

    Added mainScene of StudyProject

---
+++ StudyProject/Assets/Scripts/Main Scene/DetectClicks.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DetectClicks : MonoBehaviour {
+
+    public GameObject buttons, m_cube, spawnBlocks, first_SpBlock;
+    public Text playTxt, gameName;
+
+    private bool clicked;
+
+    void OnMouseDown()
+    {
+        if (!clicked)
+        {
+            clicked = true;
+            playTxt.gameObject.SetActive(false);
+            gameName.text = "0";
+            buttons.GetComponent<ScrollObjects>().speed = -10f;
+            buttons.GetComponent<ScrollObjects>().checkPos = -100f;
+            m_cube.GetComponent<Animation>().Play("StartGameCube");
+
+            Instantiate(first_SpBlock, new Vector3(-3f, 1.4f, 0f), Quaternion.identity);
+        }
+    }
+}
---
+++ StudyProject/Assets/Scripts/Main Scene/MainButtons.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainButtons : MonoBehaviour {
+
+    void OnMouseDown() {
+        transform.localScale = new Vector3(1.10f, 1.10f, 1.10f);
+    }
+
+    void OnMouseUp(){
+        transform.localScale = new Vector3(1f, 1f, 1f);
+    }
+}
